b83e25dae081db3633cff95573846e8d403ae2a5
Nutan Bhoyar <nutan@fple.in>
2025-04-17 12:53:16 +0530

    Project  Initialization

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Route::middleware('api')
+        //     ->prefix('admin-api') // <-- URL will start with /admin-api
+        //     ->group(base_path('routes/api-admin.php'));
+    }
+}
---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Route::middleware('api')
+            ->prefix('admin-api') // <-- URL will start with /admin-api
+            ->group(base_path('routes/api-admin.php'));
+    }
+}
